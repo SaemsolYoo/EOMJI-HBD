acf4c270834d469e84447f96d33f03c26e4d7bb2
ssaammyy <55077107+SaemsolYoo@users.noreply.github.com>
2020-10-01 02:47:42 +0900

    Add files via upload

--- modal.js
+++ modal.js
@@ -1,17 +1,17 @@
-// const btns = document.querySelector(".btns");
-// const modal = btns.querySelector(".modal");
-// const modalBtn = btns.querySelector(".btn");
+const btns = document.querySelector(".btns");
+const modal = btns.querySelector(".letter");
+const modalBtn = btns.querySelector(".btn");
 
-// // modalBtn.addEventListener("click", function (event) {
-// //   if (this.showing) {
-// //     modal.classList.remove("showing");
-// //     modalBtn.innerText = "💌";
-// //   } else {
-// //     modal.classList.add("showing");
-// //     modalBtn.innerText = "𝗫";
-// //   }
+modalBtn.addEventListener("click", function (event) {
+  if (this.showing) {
+    modal.classList.remove("showing");
+    modalBtn.innerText = "💌";
+  } else {
+    modal.classList.add("showing");
+    modalBtn.innerText = "📩";
+  }
 
-// //   this.showing = !this.showing;
-// // });
+  this.showing = !this.showing;
+});
 
-// // modalBtn.showing = false;
+modalBtn.showing = false;
